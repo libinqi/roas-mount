8d69bbe2db2f2daca1edf5220d2ca6035ff141a2
libinqi <libinqi@outlook.com>
2016-11-16 15:19:17 +0800

    add roas-mount module files

---
+++ README.md
@@ -0,0 +1,15 @@
+# roas-mount
+ 
+ Dynamically loading the module in the directory
+
+## Install
+
+    npm install --save roas-mount
+
+## Usages
+
+```
+var $modules = require('roas-mount')(__dirname);
+console.log($modules);
+
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+
+function mount(dir) {
+    if (!dir) return null;
+
+    if (!fs.existsSync(dir)) {
+        throw "roas-mount error: " + dir + ",not found";
+    }
+
+    var requires = {};
+
+    fs
+        .readdirSync(dir)
+        .filter(function (file) {
+            return (file.indexOf('.') !== 0) && (file.slice(-3) === '.js');
+        })
+        .forEach(function (file) {
+            var fileName = file.substring(0, file.indexOf('.'));
+            var m = require(dir + '/' + file);
+            if (m) {
+                requires[fileName] = m;
+                console.log('mount completed:' + dir + '/' + file);
+            }
+        });
+
+    return requires;
+}
+
+module.exports = mount;
