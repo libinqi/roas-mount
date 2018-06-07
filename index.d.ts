a2e394b1861e2a755c9c6998d946c222d1cbaca6
libinqi <libinqi@outlook.com>
2018-06-07 16:44:23 +0800

    提交ts声明文件

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+
+/* =================== USAGE ===================
+
+    import * as mount from 'roas-mount';
+
+    mount(dir)
+
+ =============================================== */
+
+declare function mount(dir: string): any;
+declare namespace mount { }
+export = mount;
